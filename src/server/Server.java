69653265393f953cf1955e257e2af229fbfb4e05
Will <wrbrown@aggies.ncat.edu>
2020-02-25 11:50:57 -0500

    Response functionality

--- src/server/Server.java
+++ src/server/Server.java
@@ -8,24 +8,27 @@ public class Server {
     private Socket socket = null;
     private ServerSocket serverSocket = null;
     private DataInputStream input = null;
+    private DataOutputStream output = null;
     
     public Server(int port) throws IOException{
         serverSocket = new ServerSocket(port);
-        System.out.print("Server started. Waiting for client.");
+        System.out.println("Server started. Waiting for client.");
         socket = serverSocket.accept();
-        System.out.print("Client connected.");
+        System.out.println("Client connected.");
         input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        output = new DataOutputStream(socket.getOutputStream());
         
         String userInput = "";
         
         while(!userInput.equals("Disconnect")){
             userInput = input.readUTF();
             System.out.println(userInput);
+            output.writeUTF("Server received " + userInput);
         }
         
         socket.close();
         input.close();
-        System.out.print("Connection closed");
+        System.out.println("Connection closed");
     }
 
     public static void main(String[] args) throws IOException {
