9035d2ca0658a8c7642a64e55be10c7e01892df8
Will <wrbrown@aggies.ncat.edu>
2020-02-25 19:43:49 -0500

    Username checking

--- src/server/Server.java
+++ src/server/Server.java
@@ -21,23 +21,42 @@ public class Server {
         System.out.println("Client connected.");
         input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
         output = new DataOutputStream(socket.getOutputStream());
+        users = new ArrayList<>();
         
         String userInput = "";
         
         while(!userInput.equals("Disconnect")){
             userInput = input.readUTF();
-            System.out.println(userInput);
-            output.writeUTF("Server received " + userInput);
+            if(SearchForUsername(userInput)){
+                output.writeUTF("Username found.");
+            }else{
+                output.writeUTF("Username NOT found, registering new username.");
+                RegisterUser(userInput);
+            }
         }
         
         socket.close();
         input.close();
         System.out.println("Connection closed");
     }
+    
+    private boolean SearchForUsername(String userInput){
+        boolean found = false;
+        for(int i = 0; i < users.size(); i++){
+            if(users.get(i).getUserName().equals(userInput)){
+                found = true;
+            }
+        }
+        return found;
+    }
+    
+    private void RegisterUser(String userInput){
+        User user = new User();
+        user.setUserName(userInput);
+        users.add(user);
+    }
 
     public static void main(String[] args) throws IOException {
-        GUILogIn gui = new GUILogIn();
-        gui.setVisible(true);
         Server server = new Server(5000);
     }
     
--- src/server/User.java
+++ src/server/User.java
@@ -4,10 +4,73 @@ public class User {
     private String userName, password, privilege;
     private int ID;
     
+    public User () {
+        userName = "";
+        password = "";
+        privilege = "";
+        ID = 0;
+    }
+    
     public User (String userName, String password, String privilege, int ID){
         this.userName = userName;
         this.password = password;
         this.privilege = privilege;
+        this.ID = ID;
+    }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the privilege
+     */
+    public String getPrivilege() {
+        return privilege;
+    }
+
+    /**
+     * @param privilege the privilege to set
+     */
+    public void setPrivilege(String privilege) {
+        this.privilege = privilege;
+    }
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
         this.ID = ID;
     }
 }
